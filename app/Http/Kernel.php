c574a8f1bb893f872c4da60050ea829bc560d85c
hubiteng <ht19871106@163.com>
2017-07-04 09:12:11 +0800

    fix kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,7 +59,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        'merchant' => \App\Http\Middleware\MerchantMiddleware::class
+        'merchant' => \App\Http\Middleware\MerchantMiddleware::class,
         'personal'=>\App\Http\Middleware\PersonalMiddleware::class,
     ];
 }
