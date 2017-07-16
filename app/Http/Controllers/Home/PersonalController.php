c3247dff9ec3413089702cdaec2303f3021b53db
lisi <wtz0324@sina.com>
2017-07-16 20:42:33 +0800

    7.16

--- app/Http/Controllers/Home/PersonalController.php
+++ app/Http/Controllers/Home/PersonalController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Home;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Personal;
-use App\Models\Order;
+use App\Models\Orders;
 use App\Models\Order_details;
 use App\Models\User_info;
 use App\Models\Collect;
@@ -18,73 +18,50 @@ use Session;
 
 class PersonalController extends Controller
 {
-<<<<<<< HEAD
-    public $id=\Session::get('user')->userid;
-    function __construct()
-    {
-    	$this->id=$id;
-    }
-    public function index()
-    {
-		$list = Personal::where('userid',$this->id)->first();
-		$order=order::where(['userid','=',$this->id],['status','=','0'])->orderBy('addtime','rsort')->get();
-		return view('home.personal.personal' ,['list'=>$list,'order'=>$order]);
-=======
+
     public function index(Request $request)
 	{
 		
 		//$list = Personal::find();		
         $user = $request->session()->get("user");
 		//dd($user);
-	    $order = order::where('userid',$user->id)->orderBy('create_time','rsort')->get();
+	    $order = Orders::where('userid',$user->id)->orderBy('create_time','rsort')->get();
 		//dd($order);
 
-		$login_user = login_user::where('username',$login->id)->get();
+		//$login_user = login_user::where('username',$login->id)->get();
 		
 		
 		$info = personal::first();
 		return view('home.personal.personal' ,['user'=>$user,'order'=>$order ,'info'=>$info]);
->>>>>>> caa86f485bdf017c11d2b4fce2142c8fcfcd6d85
+
 	}
 
- 
-    /* public function index($id)
-	{
-		$userid = user_info::where('userid',$id)->first();
-		
-		$order = order::where('userid',$id)->get();
-		
-		return view('home.personal.personal' ,['userid'=>$userid , 'order'=>$order]);
-	} */
 	
-	public function order()
+	public function order(Request $request)
 	{
-<<<<<<< HEAD
-		$order=Order::where(['userid','=',$this->id],['status','=','0'])->first();
-=======
-		$userid = \Session::get("user")->id();
-
-		$order=Order::where('userid',$userid)->get();
->>>>>>> caa86f485bdf017c11d2b4fce2142c8fcfcd6d85
+		//var_dump($request->session());
+		$id=$request->session()->get('user')->id;
+		$order=Orders::where(['userid'=>$id,'status'=>1])->get();
+		//var_dump($order);
 		return view('home.personal.order',['order'=>$order]);
 	}
 
      //未付款
-	public function orderUnrated()
+	public function orderUnrated(Request $request)
 	{
-		$orderUnrated=Order::where(['userid','=',$this->id],['status','=','1'])->get();
+		$orderUnrated=Orders::where(['userid','=',$this->id],['status','=','1'])->get();
 		return view('home.personal.orderUnrate',['orderUnrated'=>$orderUnrated]);
 	}
 
      //订单退款
-	public function orderRefund()
+	public function orderRefund(Request $request)
 	{
-		$orderRefund=Order::where(['userid','=',$this->id],['status','=','2'])->get();
+		$orderRefund=Orders::where(['userid','=',$this->id],['status','=','2'])->get();
 		return view('home.personal.orderRefund',['orderRefund'=>$orderRefund]);
 	}
 
 	//我的红包
-	public function red_packet()
+	public function red_packet(Request $request)
 	{
 		$packet=packet::where('userid',$this->id)->get();
 		return view('home.personal.packet',['packet'=>$packet]);
@@ -97,27 +74,27 @@ class PersonalController extends Controller
 	}
 */
     //评分/积分
-	public function score()
+	public function score(Request $request)
 	{
 		$score=score::where('userid',$this->id)->get();
 		return view('home.personal.score',['score'=>$score]);
 	}
 
 	//收集
-	public function collect()  
+	public function collect(Request $request)  
 	{
 		$collect=Collect::where('userid',$this->id)->get();
 		return view('home.personal.collect',['collect'=>$collect]);
 	}
 
-	public function userInfo()
+	public function userInfo(Request $request)
 	{
 		$userInfo=User_info::find('userid',$this->id)->first();
 		return view('home.personal.userinfo',['userInfo'=>$userInfo]);
 	}
 
 	//地址
-	public function address()
+	public function address(Request $request)
 	{
 		$address=Address::where('userid',$this->id)->all();
 		return view('home.personal.address',['address'=>$address]);
--- storage/framework/views/5ca4d3bdc9de82771b60ea48ff9052a076c07830.php
+++ storage/framework/views/5ca4d3bdc9de82771b60ea48ff9052a076c07830.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html ng-app="eleme" perf-error="desktop/profile/" class="ng-scope">
 <head>
-	<style type="text/css">@charset "UTF-8";
+	<style type="text/css">@charset  "UTF-8";
 		[ng\:cloak], [ng-cloak], [data-ng-cloak], [x-ng-cloak], .ng-cloak, .x-ng-cloak, .ng-hide {
 			display: none !important;
 		}
@@ -50,9 +50,9 @@
 	<link rel="icon" href="//static2.ele.me/eleme/desktop/media/img/favicon-32x32.png" type="image/png" sizes="32x32">
 	<link rel="icon" href="//static2.ele.me/eleme/desktop/media/img/favicon.png" type="image/png" sizes="96x96">
 
-	<link href="{{asset('css/vendor.0cb970.css')}}" rel="stylesheet">
+	<link href="<?php echo e(asset('css/vendor.0cb970.css')); ?>" rel="stylesheet">
 
-	<link href="{{asset('css/profile.d8f427.css')}}" rel="stylesheet">
+	<link href="<?php echo e(asset('css/profile.d8f427.css')); ?>" rel="stylesheet">
 
 
 	<!--[if lte IE 8]>
@@ -275,16 +275,16 @@
 					<div class="profile-infoitem"><a class="inherit" href="/personal/red_packet"><p>我的红包</p>
 					
 							<p class="profile-infoitem-number hongbao">
-                      <span class="number ng-binding" ng-bind="hongbaocount">{{ $info->red_packet }}</span>个</p></a>
+                      <span class="number ng-binding" ng-bind="hongbaocount"><?php echo e($info->red_packet); ?></span>个</p></a>
 					</div>
 					
 					<div class="profile-infoitem"><a class="inherit" href="/personal/score"><p>我的积分</p>
-							<p class="profile-infoitem-number score"><span class="number ng-binding" ng-bind="user.point">{{ $info->score }}</span>分</p></a>
+							<p class="profile-infoitem-number score"><span class="number ng-binding" ng-bind="user.point"><?php echo e($info->score); ?></span>分</p></a>
 					</div>
 				
 					<div class="profile-infoitem"><a class="inherit" href="/personal/balance"><p>账户余额</p>
 							<p class="profile-infoitem-number balance"><span class="number ng-binding"
-							                                                 ng-bind="user.balance| number : 2">{{ $info->balance }}</span>元
+							                                                 ng-bind="user.balance| number : 2"><?php echo e($info->balance); ?></span>元
 							</p></a>
 					</div>
 				
@@ -309,25 +309,25 @@
 		<div class="orderblock-item orderblock-rstinfo clearfix">
 		<a class="logo" ng-href="/shop/895073" href="/shop/895073">
 		<img ng-src="//fuss10.elemecdn.com/f/0b/d125b19f4e83cdd2f4294bcd11ee4jpeg.jpeg?imageMogr2/thumbnail/70x70/format/webp/quality/85" alt="商家 LOGO" src="//fuss10.elemecdn.com/f/0b/d125b19f4e83cdd2f4294bcd11ee4jpeg.jpeg?imageMogr2/thumbnail/70x70/format/webp/quality/85"></a>
-		@foreach( $order as $dd )
+		<?php $__currentLoopData = $order; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $dd): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 		 <h3 class="name">
 		 <a class="inherit ng-binding" ng-bind="order.restaurant_name"
-		 ng-href="/shop/895073" href="">{{ $dd->shop_name }}</a></h3>
+		 ng-href="/shop/895073" href=""><?php echo e($dd->shop_name); ?></a></h3>
 			<p class="product ng-binding" ng-bind="order.product">麻辣花蛤（500g）2份 / 麻辣蛏子（500g）1份</p>
 			<a class="productnum" ng-href="order/id/1209253485556894798" href="order/id/1209253485556894798">共
-			<i class="count ng-binding" ng-bind="order.productnum">{{ $dd->goods_num }}</i>个菜品&gt;</a>
+			<i class="count ng-binding" ng-bind="order.productnum"><?php echo e($dd->goods_num); ?></i>个菜品&gt;</a>
 			</div>
-			<div class="orderblock-item orderblock-time ng-binding">{{ $dd->over_time }}</div>
+			<div class="orderblock-item orderblock-time ng-binding"><?php echo e($dd->over_time); ?></div>
 			<div class="orderblock-item orderblock-price">
-			<p class="total ng-binding" ng-bind="'￥' + (order.total | number:2)">￥{{ $dd->amount }}</p>	
-			<span class="ng-binding">{{ $dd->pay }}</span></div>
+			<p class="total ng-binding" ng-bind="'￥' + (order.total | number:2)">￥<?php echo e($dd->amount); ?></p>	
+			<span class="ng-binding"><?php echo e($dd->pay); ?></span></div>
 			<div class="orderblock-item orderblock-status">
 			<p class="status ng-binding end" ng-class="{'waitpay': (order.realStatus === 4),'end': (order.realStatus === 5)}"
-			 ng-bind="order.statusText">{{ $dd->status }}</p>
+			 ng-bind="order.statusText"><?php echo e($dd->status); ?></p>
 			 <a class="statuslink ng-binding" ng-href="order/id/1209253485556894798"
 				ng-bind="order.realStatus === 4 ? '立即评价' : '订单详情'"
 				href="order">订单详情</a></div>
-	   @endforeach
+	   <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 		</div>
 		
 		
